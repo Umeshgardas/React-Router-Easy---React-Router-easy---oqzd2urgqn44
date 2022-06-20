a186dcde89e02257a47da4bb42280625ca7a75ad
Umeshgardas <87014776+Umeshgardas@users.noreply.github.com>
2022-06-20 09:51:51 +0530

    2

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Home } from "./Pages/Home";
 import { Index } from "./Pages/Index";
 import { NotFound } from "./Pages/NotFound";
+import { Routes, Route } from "react-router-dom";
 
 export const AppRoutes = () => {
   return (
